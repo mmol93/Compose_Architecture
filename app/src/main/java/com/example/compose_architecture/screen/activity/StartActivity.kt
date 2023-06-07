357181a293e8801ea5e1c9cfa7fe8e2e967b2c8e
jlee-cq <j.lee@caraquri.com>
2023-06-07 15:20:20 +0900

    simple SplashActivity is finished

--- app/src/main/java/com/example/compose_architecture/screen/activity/StartActivity.kt
+++ app/src/main/java/com/example/compose_architecture/screen/activity/StartActivity.kt
@@ -3,18 +3,51 @@ package com.example.compose_architecture.screen.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.compose_architecture.screen.UiState
 import com.example.compose_architecture.ui.theme.Compose_ArchitectureTheme
+import com.example.compose_architecture.viewModel.StartActivityViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class StartActivity : ComponentActivity() {
+
+    val viewModel: StartActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val splashScreen = installSplashScreen()
+        var uiState: UiState by mutableStateOf(UiState.Loading)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState
+                    .onEach {
+                        uiState = it
+                    }.collect()
+            }
+        }
+
+        splashScreen.setKeepOnScreenCondition {
+            when (uiState) {
+                UiState.Loading -> true
+                is UiState.Success -> false
+            }
+        }
+
+        // set UI showing after Splash
         setContent {
             // Turn off the decor fitting system windows, which allows us to handle insets,
             // including IME animations
