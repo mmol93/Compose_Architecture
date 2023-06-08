163a67707d792c6d86cdb8cf0752021ad36722a6
ljws93 <ljws93@naver.com>
2023-06-08 16:50:04 +0900

    StartActivity에서 MainActivity 열기 1. viewPager의 버튼 클릭 시 startActivity로 연다 2. StartActivity의 viewPager에서 마지막 페이지라면 버튼을 보여주게한다.

---
+++ app/src/main/java/com/example/compose_architecture/navigation/Screens.kt
@@ -0,0 +1,16 @@
+package com.example.compose_architecture.navigation
+
+enum class Screens {
+    Start,
+    Main;
+
+    companion object {
+        fun fromRoute(route: String?): Screens =
+            when (route?.substringBefore("/")) {
+                Start.name -> Start
+                Main.name -> Main
+                null -> Main
+                else -> throw IllegalArgumentException("Route $route is not recognized.")
+            }
+    }
+}
