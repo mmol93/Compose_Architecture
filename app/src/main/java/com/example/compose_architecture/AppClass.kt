8e2fd293c7be6a6b4c8ef0e910e563a1d32a832b
mmol93 <ljws93@naver.com>
2023-07-25 13:22:43 +0900

    Timber 추가

--- app/src/main/java/com/example/compose_architecture/AppClass.kt
+++ app/src/main/java/com/example/compose_architecture/AppClass.kt
@@ -2,7 +2,12 @@ package com.example.compose_architecture
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class AppClass : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(MyTimber())
+    }
 }
